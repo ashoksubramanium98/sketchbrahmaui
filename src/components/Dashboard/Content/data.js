c50bfc1f1a3be3186f59c41ab78b1564a0c51614
AshokSubramanium98 <ashok@dev-bee.com>
2021-08-20 22:02:34 +0530

    #Fix split components for dashboard

---
+++ src/components/Dashboard/Content/data.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Data = () => (
+    <div>
+
+    </div>
+);
+
+export default Data;
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
 import SideNavigation from './SideNavigation';
 import Header from './Header';
+import Menu from './Content/menu';
+import Data from './Content/data';
 
 const Dashboard = () => {
     return(
@@ -8,6 +11,20 @@ const Dashboard = () => {
             <SideNavigation />
             <div className="d-flex flex-column width-100">
                 <Header />
+                <div className="p-1">
+                    <div className="mt-1" style={{marginBottom: '5px'}}>
+                        <span className="c-pointer text-info">Dashboard</span>
+                        <span> / </span>
+                        <span className="c-pointer text-info">Test Dashboard 1</span>
+                        <span> / </span>
+                        <span>AutoRCA</span>
+                    </div>
+                    <h2 className="d-flex align-items-center"><FiArrowLeft />Test Dashboard 1</h2>
+                </div>
+                <div className="d-flex">
+                    <Menu />
+                    <Data />
+                </div>
             </div>
         </div>
     );
