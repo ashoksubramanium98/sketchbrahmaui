ffffece58087c037cd03069a5891a271d9480a1b
AshokSubramanium98 <ashok@dev-bee.com>
2021-08-20 17:59:15 +0530

    #Add home page setup

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Home = ({history}) => (
-    <>
-        Home
-    </>
+    <div className="page d-flex flex-column align-items-center justify-content-center">
+        <h4 className="mt-0">Select any one of the option to view</h4>
+        <div className="d-flex">
+            <button className="btn btn-primary mr-1" onClick={() => history.push('/api-integration')}>API Task</button>
+            <button className="btn btn-primary" onClick={() => history.push('/ui-task')}>UI Task</button>
+        </div>
+    </div>
 );
 
 export default Home;
